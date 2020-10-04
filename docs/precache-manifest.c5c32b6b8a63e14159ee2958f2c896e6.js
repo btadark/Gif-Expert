1ff30222f0fada36236b8ed2a7ddd2c0bece3bae
Brandon Ticona <jbticona2@gmail.com>
2020-10-04 18:42:20 -0400

    Docs agregado

---
+++ docs/precache-manifest.c5c32b6b8a63e14159ee2958f2c896e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869441a430316ea66e41a760c9f7ee8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b27c3d342aba450aa16",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "0a4ae2f818efcc3212eb",
+    "url": "/static/css/main.21701075.chunk.css"
+  },
+  {
+    "revision": "5b27c3d342aba450aa16",
+    "url": "/static/js/2.7c9c33fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c9c33fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4ae2f818efcc3212eb",
+    "url": "/static/js/main.7bce3a02.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
